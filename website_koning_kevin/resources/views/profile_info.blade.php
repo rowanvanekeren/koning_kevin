245d8003ce487a0ce26482cf5c49e71308c621b3
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-11-24 23:03:34 +0100

    responsive probleem opgelost

--- website_koning_kevin/resources/views/profile_info.blade.php
+++ website_koning_kevin/resources/views/profile_info.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.app')
-
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{url('/css/profile-info.css')}}">
+@stop
 @section('content')
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading text-center"><strong>INLOGGEN</strong></div>
+                <div class="panel-body">
             <h1>Dashboard</h1>
             <p>Profiel info</p>
-            
-            <div>
+            <div class="col-md-12  ">
+                <div class="col-md-12">
+                    <img class="profile-info-image" src="{{asset('images/profile_pictures/1479247671anton CV foto.jpg' /*. Auth::user()->url*/ ) }}">
+                </div>
+                <div class="col-md-12 profile-info-header">
+                    <h1 class="">{{$user->first_name}} {{$user->last_name}}</h1>
+                </div>
+            </div>
+            <div class="col-md-6">
+
                 {{$user->first_name}}
                 {{$user->last_name}}<br>
                 {{$user->email}}<br>
@@ -19,8 +32,11 @@
                 {{$user->job}}<br>
                 {{$user->job_function}}<br>
             </div>
-            <div>
+            <div class="col-md-6">
                 Adminstratieve gegevens -> checken of er al administratieve gegevens aan deze user gelinkt zijn
+            </div>
+            </div>
+            </div>
             </div>
         </div>
     </div>
