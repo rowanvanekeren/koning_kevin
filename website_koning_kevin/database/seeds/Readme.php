c9d565d196dbac32cc14055f49f2d382afd73dc6
Anton-Patokin <patokin.anton@gmail.com>
2016-12-02 12:16:26 +0100

    seeder bug server test

--- website_koning_kevin/database/seeds/Readme.php
+++ website_koning_kevin/database/seeds/Readme.php
@@ -11,7 +11,7 @@ class Readme extends Seeder
      */
     public function run()
     {
-        DB::table('readmes')->delete();
+        
 
             
 
--- website_koning_kevin/database/seeds/ReadmeTableSeeder.php
+++ website_koning_kevin/database/seeds/ReadmeTableSeeder.php
@@ -12,12 +12,6 @@ class ReadmeTableSeeder extends Seeder
     public function run()
     {
         //
-        $readme_row =
-            array(
-                'url' => '/readme.pdf',
-                'created_at' => \Carbon\Carbon::now(),
-                'updated_at' => \Carbon\Carbon::now(),
-            );
-        DB::table('readmes')->insert($readme_row);
+        
     }
 }
