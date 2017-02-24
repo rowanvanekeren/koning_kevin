1ad5a8ac99589736db445d11ce7142a1540b4392
Anton-Patokin <patokin.anton@gmail.com>
2017-02-24 22:07:27 +0100

    update mail

--- website_koning_kevin/resources/views/email/User_accepted_for_project.blade.php
+++ website_koning_kevin/resources/views/email/User_accepted_for_project.blade.php
@@ -6,9 +6,13 @@
 <body>
 
 
-<h1>Geachte {{$user->first_name}}</h1>
-<H2>U bent geaccepteerd voor {{$project}} project als vrijwilliger.</H2>
-<p>MVG</p>
+<p>Dag {{$user->first_name}}</p>
+<p>Je bent maakt nu deel uit van het project {{$project}}.<br>
+    Op de pagina van het project zelf vind je meer info over het project terug, alsook de andere vrijwilligers die deel uitmaken van het project. <br>
+    Als je hier nog vragen over hebt, neem gerust contact met ons op.
+
+</p>
+<p>Speelse groeten!</p>
 <p>Koning Kevin</p>
 
 </body>
